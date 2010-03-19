c1bc67a03d5163071aa3c994bdeeb4bd3d368e8a
robmckinnon <rob@theyworkforyou.co.nz>
2010-03-19 15:27:50 +0000

    added compass susy sass layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+
   map.resources :submitters
 
+  map.root :controller => "application"
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
