dfa9a744f985f2b0e099577fb34ee2cabc1975dc
Chris <chris@david-taggarts-macbook.local>
2010-03-20 15:45:08 +0000

    Final go!

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -3,11 +3,18 @@ raise "Sorry, you must have #{config_file}" unless File.exists?(config_file)
 
 config_options = YAML.load_file(config_file) 
 
-require 'smtp_tls'
+require 'tlsmail'
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.default_charset = "utf-8"
+
+ActionMailer::Base.raise_delivery_errors = true
 
 ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => "587",
-  :domain => "tellusonce.govtrace.com",
-  :authentication => :plain,
+  :domain          => "todemo@gmail.com",
+  :address         => 'smtp.gmail.com',
+  :port            => 587,
+  :tls             => true,
+  :authentication  => :plain
 }.merge(config_options) # Configuration options override default options
