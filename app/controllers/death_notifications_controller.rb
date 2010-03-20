5f97d29d4c134052dd36b176dad12a223133a5c0
robmckinnon <rob@theyworkforyou.co.nz>
2010-03-20 01:23:33 +0000

    moved Date functions to controller

--- app/controllers/death_notifications_controller.rb
+++ app/controllers/death_notifications_controller.rb
@@ -2,6 +2,11 @@ class DeathNotificationsController < ResourceController::Base
   
   def new
     @death_notification = DeathNotification.new
+    @latest_death_year = Date.today.year
+    @earliest_death_year = Date.today.year - 1
+    
+    @earliest_birth_year = Date.today.year - 130
+    @latest_birth_year = Date.today.year
   end
 
 end
